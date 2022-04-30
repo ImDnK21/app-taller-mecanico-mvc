9754756b416f8e7d53572d7fc7d78c93f7057f50
diegoeffar <diegoeffar@gmail.com>
2022-04-29 23:05:28 -0400

    app_name

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
+define("APP_NAME", "Taller Mecanico");
 define("APP_URL", "https://localhost/app-taller-mecanico-mvc/");
 define("controller_default", "usuarioController");
 define("action_default", "index");
-
